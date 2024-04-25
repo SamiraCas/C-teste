74f5938bbc3f5ef01360f1cd79802ea43437101f
Samira Castro <samiracastrosjn23@gmail.com>
2024-04-25 00:39:32 -0300

    treinando como funciona os metodos com e sem estados

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#");
+﻿// uma instância de uma classe
+// 'new' = operador (Resposavel por criar a instancia de uma classe) / 'Random' = classe / 'dice' = variavel
+Random dice = new Random();
+// Métodos com estado, pq ele precisa de uma variavel 
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
+Random dice = new Random();
+int roll = dice.Next();
